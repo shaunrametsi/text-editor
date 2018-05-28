36d2dfdc0b2e145b5424b1e5ccde34fbc5b742c6
Shaun Rametsi <shaunus111@gmail.com>
2018-05-28 09:32:11 +0200

    initial commit

---
+++ OpenFileDialogStreaming/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace OpenFileDialogStreaming
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnOpenFile.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFile.Location = new System.Drawing.Point(511, 8);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(143, 33);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "OPEN FILE";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSaveFile.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveFile.Location = new System.Drawing.Point(511, 47);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(143, 33);
+            this.btnSaveFile.TabIndex = 2;
+            this.btnSaveFile.Text = "SAVE FILE";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 8);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(493, 279);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 299);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ OpenFileDialogStreaming/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OpenFileDialogStreaming
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.Clear();
+
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            OpenFileDialog Open = new OpenFileDialog();
+            Open.ShowDialog();
+            StreamReader read = new StreamReader(Open.FileName);
+            string line = read.ReadLine();
+            while (line != null)
+            {
+                richTextBox1.AppendText(line + "\n");
+                line = read.ReadLine();
+            }
+
+            read.Close();
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            int K = 0;
+            SaveFileDialog save = new SaveFileDialog();
+            save.ShowDialog();
+            StreamWriter write = new StreamWriter(save.FileName);
+            write.WriteLine(DateTime.Now.ToString());
+            string line;
+            while(K < richTextBox1.Lines.Count())
+            {
+                line = richTextBox1.Lines[K];
+                write.WriteLine(line);
+                K++;
+            }
+            write.Close();
+            
+
+            
+
+        }
+    }
+}
